7927fc74f5bbdc33e7aeaa750e068b1743aeaf1f
reklast <reklast51@gmail.com>
2023-11-26 15:48:35 +0200

    Create player to play the alarm when time matches

---
+++ src/components/ChasePlayer/ChasePlayer.vue
@@ -0,0 +1,27 @@
+<script setup lang="ts">
+import { store } from '@/store/store'
+
+import chase from '@/media/chase.mp4'
+
+import styles from './ChasePlayer.module.scss'
+
+const convertOpts: object = {
+  hour: '2-digit',
+  hour12: false,
+  minute: '2-digit'
+}
+</script>
+
+<template>
+  <div :className="styles.playerContainer">
+    <video
+      v-if="
+        store.alarmTime && store.realTime.toLocaleTimeString('en', convertOpts) >= store.alarmTime
+      "
+      :className="styles.player"
+      loop
+      :src="chase"
+      autoplay
+    />
+  </div>
+</template>
