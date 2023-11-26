79a1c23dba18f30c88574a4c57c47236108970aa
reklast <reklast51@gmail.com>
2023-11-26 15:49:07 +0200

    create clock component for current time display

---
+++ src/components/ClockComponent/ClockComponent.vue
@@ -0,0 +1,29 @@
+<script setup lang="ts">
+import { store } from '@/store/store'
+import { watchEffect } from 'vue'
+
+watchEffect(() => {
+  const timer = setInterval(() => {
+    store.realTime = new Date()
+  }, 1000)
+
+  return () => {
+    clearInterval(timer)
+  }
+})
+</script>
+
+<template>
+  <div>
+    <h1>
+      {{
+        store.realTime.toLocaleTimeString('en', {
+          hour: '2-digit',
+          hour12: false,
+          minute: '2-digit',
+          second: '2-digit'
+        })
+      }}
+    </h1>
+  </div>
+</template>
