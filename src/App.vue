980f71f7d1492761aec92bc38afd07a893ca032b
reklast <reklast51@gmail.com>
2023-11-26 15:47:02 +0200

    import all future components and create a structure

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+import AlarmForm from './components/AlarmForm/AlarmForm.vue'
+import ChasePlayer from './components/ChasePlayer/ChasePlayer.vue'
+import ClockComponent from './components/ClockComponent/ClockComponent.vue'
+import FooterComponent from './components/FooterComponent/FooterComponent.vue'
+
+import styles from '@/App.module.scss'
+import '@/styles/styles.scss'
+</script>
+
+<template>
+  <div :className="styles.App">
+    <div :className="styles.upperSection">
+      <h1 :className="styles.header">CHASE ALARM</h1>
+      <ClockComponent />
+      <AlarmForm />
+    </div>
+    <div :className="styles.bottomSection">
+      <ChasePlayer />
+      <FooterComponent />
+    </div>
+  </div>
+</template>
