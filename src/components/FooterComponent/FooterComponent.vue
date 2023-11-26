86a380ebe28cfcf062cc4c2e231aae91b6690cdc
reklast <reklast51@gmail.com>
2023-11-26 15:49:19 +0200

    create footer component

---
+++ src/components/FooterComponent/FooterComponent.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+import styles from './FooterComponent.module.scss'
+</script>
+
+<template>
+  <footer :className="styles.footer">
+    <a href="https://github.com/reklast" target="_blank" rel="noreferrer" />
+  </footer>
+</template>
