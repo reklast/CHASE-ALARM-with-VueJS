39abe57f1fac9a6ce2cff31d3b08e0c0dcae2dc2
reklast <reklast51@gmail.com>
2023-11-26 15:47:57 +0200

    create Form for time input

---
+++ src/components/AlarmForm/AlarmForm.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+import { store } from '@/store/store'
+
+import styles from './AlarmForm.module.scss'
+
+let formInput: string
+
+const handleSubmit = (e: Event) => {
+  e.preventDefault()
+  store.alarmTime = formInput
+  alert(`Time set to: ${store.alarmTime}`)
+}
+</script>
+
+<template>
+  <div>
+    <form @submit="handleSubmit" :className="styles.form">
+      <input type="time" :id="styles.setTime" v-model="formInput" />
+
+      <input :id="styles.button" type="submit" value="SET" />
+    </form>
+  </div>
+</template>
